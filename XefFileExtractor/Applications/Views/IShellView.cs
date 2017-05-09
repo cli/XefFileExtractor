238a05db2d002a7ac0b759e2f5e364add6a256de
ajdillhoff <ajdillhoff@gmail.com>
2017-05-08 21:47:10 -0500

    Add project files.

---
+++ XefFileExtractor/Applications/Views/IShellView.cs
@@ -0,0 +1,11 @@
+﻿using System.Waf.Applications;
+
+namespace XefFileExtractor.Applications.Views
+{
+    internal interface IShellView : IView
+    {
+        void Show();
+
+        void Close();
+    }
+}
---
+++ XefFileExtractor/Presentation/Views/ShellWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows;
+using XefFileExtractor.Applications.Views;
+
+namespace XefFileExtractor.Presentation.Views
+{
+    [Export(typeof(IShellView))]
+    public partial class ShellWindow : Window, IShellView
+    {
+        public ShellWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
